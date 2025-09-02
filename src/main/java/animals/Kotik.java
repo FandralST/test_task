32bca5a77cba2b6e11d11ece3401b30e084d6aba
Артём <tyoma2811@gmail.com>
2025-09-02 16:19:43 +0400

    тестовое задание

---
+++ src/main/java/animals/Kotik.java
@@ -0,0 +1,185 @@
+package animals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Kotik {
+
+    private static int COUNT;
+
+    private final int METHODS = 5;
+
+    private String name;
+
+    private String voice;
+
+    private int satiety;
+
+    private int weight;
+
+    public Kotik() {
+        COUNT++;
+    }
+
+    public Kotik(String name, String voice, int satiety, int weight) {
+        this.name = name;
+        this.voice = voice;
+        this.satiety = satiety;
+        this.weight = weight;
+        COUNT++;
+    }
+
+    public boolean play() {
+        if (!checkSatiety()) {
+            return false;
+        }
+        System.out.println("LETS PLAY!");
+        this.satiety--;
+        return true;
+    }
+
+    public boolean sleep() {
+        if (!checkSatiety()) {
+            return false;
+        }
+        this.satiety--;
+        System.out.println("BEDTIME!");
+        return true;
+    }
+
+    public boolean wash() {
+        if (!checkSatiety()) {
+            return false;
+        }
+        this.satiety--;
+        System.out.println("WASHING!");
+        return true;
+    }
+
+    public boolean walk() {
+        if (!checkSatiety()) {
+            return false;
+        }
+        this.satiety--;
+        System.out.println("WALKING!");
+        return true;
+    }
+
+    public boolean hunt() {
+        if (!checkSatiety()) {
+            return false;
+        }
+        this.satiety--;
+        System.out.println("HUNTING!");
+        return true;
+    }
+
+
+    private boolean checkSatiety() {
+        if (this.satiety == 0) {
+            System.out.println("GIVE ME SOME FOOD PLS");
+            return false;
+        }
+        return true;
+    }
+
+    public void eat(int foodQuantity) {
+
+    }
+
+    public void eat(int foodQuantity, String foodName) {
+        this.satiety += foodQuantity;
+    }
+
+    public void eat() {
+        eat(4, "whiskas");
+    }
+
+
+    public List<String> liveAnotherDay() {
+        int action = 0;
+        Random rnd = new Random();
+        String actionName = "foo";
+        List<String> hourActions = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            action = rnd.nextInt(5) + 1;
+            switch (action) {
+                case 1:
+                    if (!play()) {
+                        eat();
+                        actionName = "eat";
+                    } else actionName = "play";
+                    break;
+
+                case 2:
+                    if (!sleep()) {
+                        eat();
+                        actionName = "eat";
+                    } else actionName = "sleep";
+                    break;
+
+                case 3:
+                    if (!wash()) {
+                        eat();
+                        actionName = "eat";
+                    } else actionName = "wash";
+                    break;
+
+                case 4:
+                    if (!hunt()) {
+                        eat();
+                        actionName = "eat";
+                    } else actionName = "hunt";
+                    break;
+
+                case 5:
+                    if (!walk()) {
+                        eat();
+                        actionName = "eat";
+                    } else actionName = "walk";
+                    break;
+            }
+            hourActions.add(i + " - " + actionName);
+        }
+
+        return hourActions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public static int getCOUNT() {
+        return COUNT;
+    }
+
+}
---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import animals.Kotik;
+
+import java.util.List;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Kotik firstKotik = new Kotik("Cat1", "meow", 5, 5);
+
+        Kotik secondKotik = new Kotik();
+        secondKotik.setName("Cat2");
+        secondKotik.setVoice("gav");
+        secondKotik.setSatiety(10);
+        secondKotik.setWeight(6);
+
+        List<String> firstKotikActions = firstKotik.liveAnotherDay();
+        for (String action : firstKotikActions) {
+            System.out.println(action);
+        }
+        System.out.println("Имя и вес второго котика");
+        System.out.println(secondKotik.getName());
+        System.out.println(secondKotik.getWeight());
+        System.out.println("Сравнение голосов");
+        System.out.println(compareVoice(firstKotik, secondKotik));
+        System.out.println("Количество котиков");
+        System.out.println(Kotik.getCOUNT());
+    }
+
+    static boolean compareVoice(Kotik kotik1, Kotik kotik2){
+        return kotik1.getVoice().equals(kotik2.getVoice());
+    }
+
+}
